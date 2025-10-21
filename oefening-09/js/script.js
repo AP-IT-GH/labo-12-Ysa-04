f853ee7411063807fec72a1c1f8e8fa91d6fee8e
Ysa-04 <ysaura2903@outlook.com>
2025-10-21 12:39:04 +0200

    pef 9

--- oefening-09/js/script.js
+++ oefening-09/js/script.js
@@ -1,2 +1,9 @@
 "use strict";
 
+const AMOUNT_TO_PAY = 454;
+const NUMBER_OF_50 = Math.ceil(AMOUNT_TO_PAY / 50);
+const CHANGE = (NUMBER_OF_50 * 50) - AMOUNT_TO_PAY;
+
+console.log(`Te betalen bedrag is ${AMOUNT_TO_PAY}`);
+console.log(`Je hebt ${NUMBER_OF_50} briefjes van 50 nodig`);
+console.log(`Je wisselgeld is dan ${CHANGE}`);
