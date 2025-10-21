0e46932db9166a1b8aba129d4b19cca66e103edc
Ysa-04 <ysaura2903@outlook.com>
2025-10-21 12:31:47 +0200

    oef 7&8

--- oefening-07/js/script.js
+++ oefening-07/js/script.js
@@ -7,6 +7,3 @@ let year = 7;
 let afterSevenYears = start * (1 + RENTE)**year;
 console.log(`Op een periode van 7 jaar stijgt je bezit van ${start} naar ${afterSevenYears.toFixed(0)}`);
 console.log(`Dit met een rentevoet van ${RENTE}`);
-
-
-
--- oefening-08/js/script.js
+++ oefening-08/js/script.js
@@ -1,2 +1,12 @@
 "use strict";
 
+let currentTime = 14;
+let addTime = 33;
+let result = currentTime + addTime;
+
+console.log(`Het is nu ${currentTime} uur.`);
+if (result > 24) {
+    result = result - 24;
+}
+console.log(`Over ${addTime} uur is het ${result} uur.`);
+
