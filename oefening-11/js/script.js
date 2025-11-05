bd8f2384accea8af3091d0f7d9a2d80d118b8e2d
Ysa-04 <ysaura2903@outlook.com>
2025-11-05 17:14:55 +0100

    Oef 11

--- oefening-11/js/script.js
+++ oefening-11/js/script.js
@@ -1,2 +1,9 @@
 "use strict";
 
+let email = "ysa.vanassche@ap.be";
+let endIndex = email.indexOf("@");
+let fullName = email.substring(0,endIndex).replace(".", " ").toUpperCase();
+
+console.log(`${fullName}`);
+
+
