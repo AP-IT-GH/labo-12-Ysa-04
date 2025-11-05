3085f5b566b2024bdf46d824a0a1fe00d7921408
Ysa-04 <ysaura2903@outlook.com>
2025-11-05 17:31:30 +0100

    OEf 12

--- oefening-12/js/script.js
+++ oefening-12/js/script.js
@@ -1,2 +1,10 @@
 "use strict";
 
+let firstName = "Ysa";
+let lastName = "Van Assche";
+
+let firstLetter = firstName.substring(0,1).toUpperCase();
+let lastNameCapitalize = lastName.substring(0,1).toUpperCase() + lastName.substring(1);
+
+console.log(`${firstLetter}. ${lastNameCapitalize}`);
+
