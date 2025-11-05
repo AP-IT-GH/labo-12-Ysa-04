db2e9c8ae3b9fa678352a528c1b2b08288404d3a
Ysa-04 <ysaura2903@outlook.com>
2025-11-05 16:56:57 +0100

    Oef 10

--- oefening-10/js/script.js
+++ oefening-10/js/script.js
@@ -1,2 +1,9 @@
 "use strict";
 
+let text =
+  "De winnende lottonummers van deze week: 05,09,13,18,27,44. Proficiat aan alle deelnemers.";
+
+let startIndex = text.indexOf("05");
+let endIndex = text.indexOf(".")
+let formattedLottoNumbers = text.substring(startIndex,endIndex).replaceAll(",","|");
+console.log(formattedLottoNumbers);
